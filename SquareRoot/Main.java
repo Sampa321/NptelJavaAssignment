3f386b7e2de7e0ccfbef6c136182be17c28fa1f6
Sampa Nayak <sampanayak987@gmail.com>
2025-10-20 12:53:27 +0530

    Add files via upload

---
+++ SquareRoot/Main.java
@@ -0,0 +1,12 @@
+package PracticeClass;
+
+public class Main{
+
+    public static void main(String[] args) {
+          Calculator c = new Calculator();
+          c.i=20.0;
+          c.p();
+        System.out.println("square root of "+c.i+"is : "+c.x);
+    };
+
+}
