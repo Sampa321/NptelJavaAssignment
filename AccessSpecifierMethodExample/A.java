4cf93e71f92a59209a0225d3f1a597ea5a9e0b4f
Sampa Nayak <sampanayak987@gmail.com>
2025-10-20 12:04:14 +0530

    Add class A with access specifier methods

---
+++ AccessSpecifierMethodExample/A.java
@@ -0,0 +1,13 @@
+package PracticeClass;
+
+class A {
+    public void msg1(){
+        System.out.println("Class A :public");
+    }
+    private void msg2(){
+        System.out.println("Class A :private");
+    }
+    protected void msg3(){
+        System.out.println("Class A :protected");
+    }
+}
